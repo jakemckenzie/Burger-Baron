e2992b605bc106e7c296a866430def9e3a7d9b74
jakemckenzie <jake314@uw.edu>
2018-04-01 14:45:20 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -466,9 +466,9 @@ class Burger {
 
     }
 
-    public void addIngredient(String type) {
-
-	ingredientStack.push(type);
+    public void addIngredient(String ingredient) {
+	
+	ingredientStack.push(ingredient);
 
 	swapIngredients();
 
@@ -525,7 +525,54 @@ public class Main {
     };
     
     public void testMyStack() {
-        
+
+	/*
+	 * This is my base case test.
+	 */
+
+	System.out.println("\nTest MyStack with (uniquely random) Integer type:");
+
+	MyStack<Integer> intStack = new MyStack<Integer>();
+
+	ArrayList<Integer> urn = new ArrayList<Integer>();
+
+	/*
+	 * This is my check to see whether the stack is sufficiently generic.
+	 */
+
+	for (int i = 1; i < 11; i++) urn.add(new Integer(i));
+
+	Collections.shuffle(urn);
+
+	for (int i = 0; i < 10; i++) intStack.push(urn.get(i));
+
+	for (int i = 0; i < 10; i++) System.out.println(urn.get(i));
+
+	System.out.println();
+
+	while (!intStack.isEmpty()) {
+
+	    System.out.println(intStack.peek().toString());
+
+	    intStack.pop();
+
+	}
+	
+	MyStack<String> testSize = new MyStack<String>();
+
+	System.out.println("\nAfter initialization I expect the size of MyStack<String> testSize to be zero and it is: " + testSize.size());
+	testSize.push("Test");
+
+	System.out.println("\nAfter push I expect the size of MyStack<String> testSize to be one and it is: " + testSize.size());
+	testSize.pop();
+	
+	System.out.println("\nAfter pop I expect the size of MyStack<String> testSize to be zero and it is: " + testSize.size());
+
+	/*
+	 * This is my test to see the run times of each method within MyStack.
+	 * todo: optimize push (sometimes there are unknown slowdowns)
+	 */
+	
         String string = "Call me Ishmael. Some years ago- never mind how long precisely-"+
 	    "having little or no money in my purse, and nothing particular to interest me on shore,"+
 	    "I thought I would sail about a little and see the watery part of the world. It is a way "+
@@ -545,6 +592,8 @@ public class Main {
 
         System.out.println("Initializing a stack:");
 
+	System.out.println();
+	
 	for (int i = 0; i < 10; i++) {
 
 	    startTime = System.nanoTime();
@@ -687,61 +736,183 @@ public class Main {
             System.out.println(elapsedTime + " nanoseconds elapsed");
 
 	}
-
-	System.out.println("\nTest MyStack with (uniquely random) Integer type:");
-
-	MyStack<Integer> intStack = new MyStack<Integer>();
-
-	ArrayList<Integer> urn = new ArrayList<Integer>();
-
-	for (int i = 1; i < 11; i++) urn.add(new Integer(i));
-
-	Collections.shuffle(urn);
-
-	for (int i = 0; i < 10; i++) intStack.push(urn.get(i));
-
-	for (int i = 0; i < 10; i++) System.out.println(urn.get(i));
-
-	System.out.println();
-
-	while (!intStack.isEmpty()) {
-
-	    System.out.println(intStack.peek().toString());
-
-	    intStack.pop();
-
-	}
-
-	MyStack<String> testSize = new MyStack<String>();
-
-	System.out.println("\nAfter initialization I expect the size of MyStack<String> testSize to be zero and it is: " + testSize.size());
-	testSize.push("Test");
-
-	System.out.println("\nAfter push I expect the size of MyStack<String> testSize to be one and it is: " + testSize.size());
-	testSize.pop();
-	
-	System.out.println("\nAfter pop I expect the size of MyStack<String> testSize to be zero and it is: " + testSize.size());
 	
     }
 
     public void testBurger() {
 
-    }
+	/*
+	 * This is my base cases tests.
+	 */
+	
+	long startTime;
+	
+	long endTime;
 
-    public void testParseLine() {
+	long elapsedTime;
+	
+	Burger testTheWorks = new Burger(true);
+
+	Burger testPlain = new Burger(false);
 
 	ArrayList<String> ingredientsUrn = new ArrayList<String>(Arrays.asList("Pickle",
-									    "Mayonnaise",
-									    "Baron-Sauce",
-									    "Lettuce",
-									    "Tomato",
-									    "Onions",
-									    "Pepperjack",
-									    "Mozzarella",
-									    "Cheddar",
-									    "Mushrooms",
-									    "Mustard",
-									    "Ketchup"));
+									       "Mayonnaise",
+									       "Baron-Sauce",
+									       "Lettuce",
+									       "Tomato",
+									       "Onions",
+									       "Pepperjack",
+									       "Mozzarella",
+									       "Cheddar",
+									       "Mushrooms",
+									       "Mustard",
+									       "Ketchup"));
+	
+	ArrayList<String> categoriesUrn = new ArrayList<String>(Arrays.asList("Sauce",
+									      "Cheese",
+									      "Veggies"));
+
+	ArrayList<String> pattiesUrn = new ArrayList<String>(Arrays.asList("Chicken",
+									   "Veggie",
+									   ""));
+
+	ArrayList<String> cntPattyUrn = new ArrayList<String>(Arrays.asList("Double",
+									    "Triple"
+									    ,""));
+	
+	ArrayList<String> baronBurgerUrn = new ArrayList<String>(Arrays.asList("Baron Burger",
+									       "Burger"));
+
+	shuffleUrn(ingredientsUrn);
+
+	shuffleUrn(categoriesUrn);
+
+	shuffleUrn(pattiesUrn);
+
+	shuffleUrn(cntPattyUrn);
+
+	shuffleUrn(baronBurgerUrn);
+
+	System.out.println("\nThis should be a baron burger: " + "\n" + testTheWorks.toString());
+
+	System.out.println("\nThis should be a plain burger: " + "\n" + testPlain.toString() + "\n");
+
+	for (int i = 0; i < 3; i++) {
+
+	    testPlain = new Burger(false);
+
+	    testPlain.addCategory(categoriesUrn.get(i));
+
+	    System.out.println("Testing addCategory with " + categoriesUrn.get(i) + " and the output is: \n" + testPlain.toString() + "\n");
+    	    
+	}
+
+	for (int i = 0; i < 3; i++) {
+
+	    testTheWorks = new Burger(true);
+
+	    testTheWorks.removeCategory(categoriesUrn.get(i));
+
+	    System.out.println("Testing removeCategory with " + categoriesUrn.get(i) + " and the output is: \n" + testTheWorks.toString() + "\n");
+        
+	}
+
+	System.out.println("\naddCategories():");
+	
+	for (int i = 0; i < 10; i++) {
+
+	    shuffleUrn(categoriesUrn);
+	    
+	    testPlain = new Burger(false);
+
+	    startTime = System.nanoTime();
+
+            testPlain.addCategory(categoriesUrn.get(0));
+          
+            endTime = System.nanoTime();
+
+	    elapsedTime = endTime - startTime;
+
+            System.out.println(elapsedTime + " nanoseconds elapsed");
+
+	}
+
+	System.out.println("\nremoveCategories():");
+	
+	for (int i = 0; i < 10; i++) {
+
+	    shuffleUrn(categoriesUrn);
+	    
+	    testTheWorks = new Burger(true);
+
+	    startTime = System.nanoTime();
+
+            testTheWorks.removeCategory(categoriesUrn.get(0));
+          
+            endTime = System.nanoTime();
+
+	    elapsedTime = endTime - startTime;
+
+            System.out.println(elapsedTime + " nanoseconds elapsed");
+
+	}
+
+	System.out.println("\naddIngredients():");
+	
+	for (int i = 0; i < 10; i++) {
+
+	    shuffleUrn(ingredientsUrn);
+	    
+	    testPlain = new Burger(false);
+
+	    startTime = System.nanoTime();
+
+            testPlain.addIngredient(ingredientsUrn.get(0));
+          
+            endTime = System.nanoTime();
+
+	    elapsedTime = endTime - startTime;
+
+            System.out.println(elapsedTime + " nanoseconds elapsed");
+
+	}
+
+	System.out.println("\nremoveIngredients():");
+	
+	for (int i = 0; i < 10; i++) {
+
+	    shuffleUrn(ingredientsUrn);
+	    
+	    testTheWorks = new Burger(true);
+
+	    startTime = System.nanoTime();
+
+            testTheWorks.removeCategory(ingredientsUrn.get(0));
+          
+            endTime = System.nanoTime();
+
+	    elapsedTime = endTime - startTime;
+
+            System.out.println(elapsedTime + " nanoseconds elapsed");
+
+	}
+    
+    }
+
+    public void testParseLine() throws IOException {
+
+	ArrayList<String> ingredientsUrn = new ArrayList<String>(Arrays.asList("Pickle",
+									       "Mayonnaise",
+									       "Baron-Sauce",
+									       "Lettuce",
+									       "Tomato",
+									       "Onions",
+									       "Pepperjack",
+									       "Mozzarella",
+									       "Cheddar",
+									       "Mushrooms",
+									       "Mustard",
+									       "Ketchup"));
 	
 	ArrayList<String> categoriesUrn = new ArrayList<String>(Arrays.asList("Sauce",
 									      "Cheese",
@@ -1046,7 +1217,9 @@ public class Main {
 
 	test.testMyStack();
 
-	test.testBurger()
+	test.testBurger();
+
+	//for my own usage to test parseLine().
 	
 	//test.testParseLine();
     }
